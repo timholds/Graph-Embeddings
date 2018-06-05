3fd099804f04ff3f8ae864eea53e363b8e0d829e
James W. Weis <jww@mit.edu>
2018-06-05 18:36:31 -0400

    Attempting PageRank

--- src/main/scala/QuantaGraph/QuantaGraphMain.scala
+++ src/main/scala/QuantaGraph/QuantaGraphMain.scala
@@ -4,7 +4,6 @@ import QuantaGraph.QuantaStream.Quanta
 import cats.effect.IO
 import fs2.Stream
 import gremlin.scala.ScalaGraph
-
 object Main extends App {
 
   val s: Stream[IO, Quanta] = QuantaStream.getQuantaStream.take(10)
@@ -15,5 +14,12 @@ object Main extends App {
   println("Edges:")
   println(g.E().toList)
   println("Done.")
+
+  val r1 = g.traversalSource.underlying.withComputer().V().pageRank().valueMap()
+  val r2 = g.traversalSource.underlying.withComputer().V().pageRank().times(5).order().by("pagerank").valueMap()
+
+
+  println(r1)
+  println(r2)
 
 }
