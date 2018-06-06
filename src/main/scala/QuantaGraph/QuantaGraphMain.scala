d62286a4776892155df0b45ba12a44591e3cac24
James W. Weis <jww@mit.edu>
2018-06-06 00:33:23 -0400

    Working PageRank with *.test.txt

--- src/main/scala/QuantaGraph/QuantaGraphMain.scala
+++ src/main/scala/QuantaGraph/QuantaGraphMain.scala
@@ -4,6 +4,8 @@ import QuantaGraph.QuantaStream.Quanta
 import cats.effect.IO
 import fs2.Stream
 import gremlin.scala.ScalaGraph
+import org.apache.tinkerpop.gremlin.process.traversal.Order
+
 object Main extends App {
 
   val s: Stream[IO, Quanta] = QuantaStream.getQuantaStream.take(10)
@@ -13,13 +15,16 @@ object Main extends App {
   println(g.V().toList())
   println("Edges:")
   println(g.E().toList)
+
+  println("PageRank:")
+  val r = g.traversalSource.underlying.withComputer().V()
+    .pageRank().by("pageRank")
+    .order().by("pageRank", Order.decr)
+    .valueMap("pageRank", "title", "year").toList
+
+  r.forEach(v =>
+    println(v.values())
+  )
+
   println("Done.")
-
-  val r1 = g.traversalSource.underlying.withComputer().V().pageRank().valueMap()
-  val r2 = g.traversalSource.underlying.withComputer().V().pageRank().times(5).order().by("pagerank").valueMap()
-
-
-  println(r1)
-  println(r2)
-
 }
