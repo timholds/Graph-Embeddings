40af883a1ab5f8dd2da4ef596b7f688d58d31888
James W. Weis <jww@mit.edu>
2018-06-11 16:39:44 -0400

    Printing PageRank results to file

--- src/main/scala/QuantaGraph/QuantaGraphMain.scala
+++ src/main/scala/QuantaGraph/QuantaGraphMain.scala
@@ -1,10 +1,16 @@
 package QuantaGraph
 
+import java.util
+import java.util.{ArrayList => JArrayList}
+
 import cats.effect.IO
 import datastream._
 import fs2.Stream
 import gremlin.scala._
 import org.apache.tinkerpop.gremlin.process.traversal.Order
+
+import scala.collection.JavaConverters._
+
 
 object Main extends App {
 
@@ -19,9 +25,27 @@ object Main extends App {
   println("Running PageRank...")
   val pageRankResults = sg.traversalSource.underlying.withComputer().V()
     .pageRank().by("pageRank")
-    .order().by("pageRank", Order.decr).limit(20)
-    .valueMap("pageRank", "title", "year")
-    .toList.forEach(v => println(v.values()))
+    .order().by("pageRank", Order.decr)
+
+
+  val resultString: StringBuilder = new StringBuilder
+
+  def node2String(v: util.Map[String, Nothing]) = v.get("title").asInstanceOf[JArrayList[String]].get(0) + "," +
+    v.get("pageRank").asInstanceOf[JArrayList[Double]].get(0) + "\r\n"
+
+  val outputFilename = "pageRankResults.txt"
+  println("Printing results to " + outputFilename + "...")
+
+  import java.io._
+
+  val bw = new BufferedWriter(new FileWriter(new File(outputFilename)))
+  pageRankResults
+    .limit(100)
+    .valueMap("title", "pageRank")
+    .toList.asScala
+    .map(v => node2String(v))
+    .foreach(v => bw.write(v))
+  bw.close()
 
   println("Done.")
 }
--- src/main/scala/QuantaGraph/QuantaStream.scala
+++ src/main/scala/QuantaGraph/QuantaStream.scala
@@ -48,7 +48,7 @@ class QuantaStream[F[_]]()(implicit F: Effect[F]) {
   val projectRoot: String = new java.io.File(".").getCanonicalPath
   val dataFolder: String = projectRoot + "/data/test"
   val dataFiles: List[File] =
-    getListOfFiles(dataFolder).filter(f => f.getName.endsWith(".test.txt"))
+    getListOfFiles(dataFolder).filter(f => f.getName.endsWith(".txt"))
 
   import scala.concurrent.ExecutionContext.Implicits.global
 
