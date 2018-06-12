71fae5e26abb68c52b795228c34eaa8869d89071
Yitong Huang <yitong_huang@brown.edu>
2018-06-12 16:09:11 -0400

    Adding JSON fields for #4

--- src/main/scala/QuantaGraph/QuantaGraphMain.scala
+++ src/main/scala/QuantaGraph/QuantaGraphMain.scala
@@ -22,6 +22,8 @@ object Main extends App {
   val sg: ScalaGraph = BuildQuantaGraph.buildJanusGraph(s)
   println("Build graph with: \n\t" + sg.V.count.head + " Vertices\n\t" + sg.E.count.head + " Edges")
 
+  sg.V().valueMap().toList.foreach(v => println(v.get("publisher")))
+
   println("Running PageRank...")
   val numResultsToReturn = 10000
   val pageRankResults = sg.traversalSource.underlying.withComputer().V()
