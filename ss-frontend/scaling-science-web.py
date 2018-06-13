61d8e1ab43ede4651da54aeeb796442ebf9108e3
timholds <tholdsworth@sandiego.edu>
2018-06-13 17:39:25 -0400

    Added frontend code that's not working all the way yet

---
+++ ss-frontend/scaling-science-web.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index-2-bubbles.html')
+    #return render_template('index-bubble.html')
+
+
+if __name__ == '__main__':
+    app.run(port=5088)
