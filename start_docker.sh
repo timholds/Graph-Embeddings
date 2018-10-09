aa1559c9782254731dcbc62b0e8664d442039a09
James W. Weis <jww@mit.edu>
2018-10-09 10:28:59 -0400

    Updating remote repository with too many changes to type

--- start_docker.sh
+++ start_docker.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+# Create correct symbolic links to neo4j and data directories
+# (edit as appropriate for local environment)
+ln -s /dtmp/`whoami`/neo4j neo4j
+ln -s /dtmp/`whoami`/data data
+
+# Change permissions to make things easy
 chmod +x start_docker.sh
 chmod 777 -R notebooks
 chmod 777 -R neo4j
 chmod 777 -R data
 chmod 777 requirements.txt
 
+# Export some environmental variables for Docker
 export UID=$(id -u)
 export GID=$(id -g)
 export HOSTNAME=$(hostname)
 export DNSDOMAINNAME=$(dnsdomainname)
 
-echo " Setting env var..."
+# Say what we did
+echo " Created symbolic links:"
+echo "    neo4j -> /dtmp/`whoami`/neo4j"
+echo "    data -> /dtmp/`whoami`/data"
+echo " Set environmental variables:"
 echo "    UID=$UID"
 echo "    GID=$GID"
 echo "    HOSTNAME=$HOSTNAME"
